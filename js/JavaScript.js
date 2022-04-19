03ded17d2a75a25a5f8833b3a87fcae90c0b9fef
CarlosLincon <carloslincon29@gmail.com>
2022-04-18 22:56:12 -0300

    Terminando o menu e começando o responsivel

--- js/JavaScript.js
+++ js/JavaScript.js
@@ -1,6 +1,37 @@
+const graduaçãoDiv = document.getElementById("GraduaçãoDiv");
 const graduaçãoLi = document.getElementById("GraduaçãoLi");
 const graduaçãoUl = document.getElementById("GraduaçãoUl");
+const academicoDiv = document.getElementById("AcademicoDiv");
+const academicoLi = document.getElementById("AcademicoLi");
+const academicoUl = document.getElementById("AcademicoUl");
+const institucionalDiv = document.getElementById("InstitucionalDiv");
+const institucionalLi = document.getElementById("InstitucionalLi");
+const institucionalUl = document.getElementById("InstitucionalUl");
+const menuOFF = document.getElementsByClassName(".menuOFF");
 
 graduaçãoLi.addEventListener("mouseover", () => {
     graduaçãoUl.classList.add("onDisplay");
+    academicoUl.classList.remove("onDisplay");
+    institucionalUl.classList.remove("onDisplay");
+});
+graduaçãoDiv.addEventListener("mouseout", () => {
+    graduaçãoUl.classList.remove("onDisplay");
+});
+
+academicoLi.addEventListener("mouseover", () => {
+    academicoUl.classList.add("onDisplay");
+    graduaçãoUl.classList.remove("onDisplay");
+    institucionalUl.classList.remove("onDisplay");
+});
+academicoDiv.addEventListener("mouseout", () => {
+    academicoUl.classList.remove("onDisplay");
+});
+
+institucionalLi.addEventListener("mouseover", () => {
+    institucionalUl.classList.add("onDisplay");
+    academicoUl.classList.remove("onDisplay");
+    graduaçãoUl.classList.remove("onDisplay");
+});
+institucionalDiv.addEventListener("mouseout", () => {
+    institucionalUl.classList.remove("onDisplay");
 });
