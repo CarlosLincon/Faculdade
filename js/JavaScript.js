87c3255ebc1566b1831ea2b13458e5746a63902d
CarlosLincon <carloslincon29@gmail.com>
2022-04-19 12:10:55 -0300

    Conclusão do Header

--- js/JavaScript.js
+++ js/JavaScript.js
@@ -7,8 +7,10 @@ const academicoUl = document.getElementById("AcademicoUl");
 const institucionalDiv = document.getElementById("InstitucionalDiv");
 const institucionalLi = document.getElementById("InstitucionalLi");
 const institucionalUl = document.getElementById("InstitucionalUl");
-const menuOFF = document.getElementsByClassName(".menuOFF");
+const element = document.getElementsByClassName("container-menu");
+const rect = element[0].getBoundingClientRect();
 
+//menu effects
 graduaçãoLi.addEventListener("mouseover", () => {
     graduaçãoUl.classList.add("onDisplay");
     academicoUl.classList.remove("onDisplay");
@@ -34,4 +36,15 @@ institucionalLi.addEventListener("mouseover", () => {
 });
 institucionalDiv.addEventListener("mouseout", () => {
     institucionalUl.classList.remove("onDisplay");
+});
+// end menu
+
+window.addEventListener("scroll", () => {
+    var y = window.scrollY;
+    if (y >= 140) {
+        console.log("adsa");
+        element[0].id = "fixed";
+    } else if (y < 140) {
+        element[0].id = " ";
+    }
 });
