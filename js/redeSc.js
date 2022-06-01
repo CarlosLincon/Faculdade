a3876cce8c8d87b8921cb7ad67c868d762b98caa
CarlosLincon <carloslincon29@gmail.com>
2022-06-01 11:37:53 -0300

    att no style da rede e no js

--- js/redeSc.js
+++ js/redeSc.js
@@ -1,20 +1,36 @@
 const rds = document.querySelector("#rede-sociais");
+const rdsh = document.querySelector("#msg");
 const imgSC = document.querySelector("#img-redeSC");
-
+var a = 0;
+var b = 1;
+var c = -1;
 rds.addEventListener("click", () => {
-    rds.classList.toggle("click")
+    rds.classList.toggle("click");
 });
 
+const iconRede = [
+    ["./Images/rede-sociais/facebook.png", "facebook"],
+    ["./Images/rede-sociais/instagram.png", "instagram"],
+    ["./Images/rede-sociais/whatsapp.png", "whatsapp"],
+];
+
 setInterval(function() {
-    // imgSC.classList.toggle("instagram");
-    // if (imgSC.className == "instagram") {
-    //     imgSC.setAttribute("src", "./Images/rede-sociais/instagram.png")
-    // } else if (imgSC.className == "facebook") {
-    //     imgSC.classList.toggle("facebook");
-    //     imgSC.setAttribute("src", "./Images/rede-sociais/facebook.png")
-    // } else {
 
-    //     imgSC.setAttribute("src", "./Images/rede-sociais/whatsapp.png")
-    // }
+    if (a == 3) {
+        a = 0;
+        c = -1;
+    }
+    if (c != -1) {
+        rdsh.classList.remove(iconRede[c][1]);
+    } else if (c == -1) {
+        rdsh.classList.remove(iconRede[2][1]);
+    }
 
-}, 2500);
+    rdsh.classList.add(`${iconRede[a][1]}`);
+    imgSC.setAttribute("src", `${iconRede[a][0]}`);
+    a++;
+    c = c + 1;
+
+}, 5500);
+
+console.table(iconRede);
