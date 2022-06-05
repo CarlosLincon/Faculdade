3ece4f3d588fdd7885d8372d5dc8829f31a12836
CarlosLincon <carloslincon29@gmail.com>
2022-06-05 18:25:55 -0300

    Iniciando os cards dos cursos

--- js/windowOn.js
+++ js/windowOn.js
@@ -1,8 +1,9 @@
 window.onload = function() {
     altura = window.screen.height;
     largura = window.screen.width;
-    let alturaDivido = altura / 2.5;
-    rds.style.transform = `translateY(${alturaDivido}px)`;
+
+    let alturaDivido = altura;
+   
     console.log(altura);
     img.classList.add("transformOp");
     alunoMenu.classList.add("transformOp");
